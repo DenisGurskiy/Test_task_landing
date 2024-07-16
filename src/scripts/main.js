a6ffd2cab76c2341863e311e890fef513e4616de
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-07-16 23:25:09 +0200

    start

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+const page = document.documentElement;
+
+const switcher = document.querySelector(".theme-switcher");
+const switcherPointer = document.querySelector(".theme-switcher__pointer");
+const switcherLight = document.querySelector(".theme-switcher__icon--light");
+const switcherDark = document.querySelector(".theme-switcher__icon--dark");
+
+switcher.addEventListener("click", () => {
+  page.classList.toggle("page--dark-theme");
+
+  if (page.classList.contains("page--dark-theme")) {
+    switcherPointer.classList.add("theme-switcher__pointer--dark");
+    switcherLight.classList.remove("theme-switcher__icon--lightActive");
+    switcherDark.classList.add("theme-switcher__icon--darkActive");
+  } else {
+    switcherPointer.classList.remove("theme-switcher__pointer--dark");
+    switcherDark.classList.remove("theme-switcher__icon--darkActive");
+    switcherLight.classList.add("theme-switcher__icon--lightActive");
+  }
+});
