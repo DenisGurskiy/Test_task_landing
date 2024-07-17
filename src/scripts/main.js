b354fb1bddaad94ce858a04f4d3dd6480dbe5a48
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-07-17 15:54:33 +0200

    alomost done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ const switcher = document.querySelector(".theme-switcher");
 const switcherPointer = document.querySelector(".theme-switcher__pointer");
 const switcherLight = document.querySelector(".theme-switcher__icon--light");
 const switcherDark = document.querySelector(".theme-switcher__icon--dark");
+const headerLinks = document.querySelectorAll(".header__link");
 
 switcher.addEventListener("click", () => {
   page.classList.toggle("page--dark-theme");
@@ -14,9 +15,27 @@ switcher.addEventListener("click", () => {
     switcherPointer.classList.add("theme-switcher__pointer--dark");
     switcherLight.classList.remove("theme-switcher__icon--lightActive");
     switcherDark.classList.add("theme-switcher__icon--darkActive");
+
+    headerLinks.forEach((link) => {
+      link.classList.add("header__link--dark");
+    });
   } else {
     switcherPointer.classList.remove("theme-switcher__pointer--dark");
     switcherDark.classList.remove("theme-switcher__icon--darkActive");
     switcherLight.classList.add("theme-switcher__icon--lightActive");
+
+    headerLinks.forEach((link) => {
+      link.classList.remove("header__link--dark");
+    });
   }
+});
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
